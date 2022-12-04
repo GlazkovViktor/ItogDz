c49dad68b33bcf05e7b9242332bf959604b4a4a6
hockeymiass21@gmail.com <hockeymiass21@gmail.com>
2022-12-04 20:01:26 +0500

    задали функцию поиска

--- Program.cs
+++ Program.cs
@@ -2,6 +2,17 @@
 Console.WriteLine("Введите массив строк через пробел");
 string stroka = Console.ReadLine();
 arr = stroka.Split(' ');
-string[] arr1;
-
+string[] arr1 = new string[arr.Length];
+void Find(string[] arr, string[] arr1)
+{
+    int k = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            arr1[k] = arr[i];
+            k++;
+        }
+    }
+}
 
